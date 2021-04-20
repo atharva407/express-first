a3f8469cfb794325db8031db271c881dcd663fc2
Atharva Pamale <atharva407@student.sfit.ac.in>
2021-04-20 10:02:42 +0530

    Added Html

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 
-
 app.get('/',(req,res)=>{
-    res.send("Hello Express");
+    absolutePath = __dirname + 'views/index.html'
+    res.sendFile(absolutePath)
 })
 
 
